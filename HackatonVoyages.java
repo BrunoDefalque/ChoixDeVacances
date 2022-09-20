d54f534368c1b426bbc6a31151446ff20bdc5de1
BrunoDefalque <defalquebruno@gmail.com>
2022-09-20 20:17:27 +0200

    changement du nom de paramètres p en p1

--- HackatonVoyages.java
+++ HackatonVoyages.java
@@ -9,19 +9,19 @@ public class HackatonVoyages {
 
 	static void ChoixDestination() {
 
-		String p;
+	//	String p1 = "chaud";
 
-		if (p.equals("chaud")) {
+		if (p1.equals("chaud")) {
 
 			System.out.println("Tu as de la chance, il faudra prévoir lunettes de soleil et crème solaire !");
 		}
 
-		else if (p.equals("froid")) {
+		else if (p1.equals("froid")) {
 
 			System.out.println(" tu as de la chance ! Tu vas certeinement skier :-) prévois quand même écharpe et bonnet");
 		}
 
-		else if (p.equals("tempéré")) {
+		else if (p1.equals("tempéré")) {
 
 			System.out.println(" tu as de la chance, tu vas découvrir des musées et des promenades ! Prévois quand même des bonnes chaussures de marche !");
 		}
